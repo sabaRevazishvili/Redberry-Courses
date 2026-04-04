e6a6db20b2f739b863d163c99a4045160d33eb1f
საბა რევაზიშვილი <revazishvilisaba37@gmail.com>
2026-04-05 02:28:34 +0400

    fix: move Navbar inside BrowserRouter and add Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Courses from "./pages/Courses";
 import CourseDetails from "./pages/CourseDetails";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <Navbar />
-
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Dashboard />}></Route>
           <Route path="/courses" element={<Courses />}></Route>
           <Route path="/courseDetails" element={<CourseDetails />}></Route>
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
