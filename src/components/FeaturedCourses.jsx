06b9a56bd9c650838b8f731e2cee1557d240d617
საბა რევაზიშვილი <revazishvilisaba37@gmail.com>
2026-04-10 21:47:00 +0400

    feat: add featured courses section with API data

--- src/components/FeaturedCourses.jsx
+++ src/components/FeaturedCourses.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FeaturedCourseCard from "./ui/FeaturedCourseCard";
 
 const FeaturedCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const [error, setError] = useState(null);
+  const URL = "https://api.redclass.redberryinternship.ge/api/courses/featured";
+  useEffect(() => {
+    fetch(URL)
+      .then((response) => response.json())
+      .then((data) => setCourses(data.data))
+      .catch((err) => setError(err.message));
+  }, []);
+  console.log(courses);
+  console.log(error);
+
   return (
     <section className="py-16">
       <h2 className="font-semibold text-4xl">Start Learning Today</h2>
@@ -9,9 +21,9 @@ const FeaturedCourses = () => {
         Choose from our most popular courses and begin your journey
       </p>
       <div className="w-full flex flex-row justify-between gap-6">
-        <FeaturedCourseCard />
-        <FeaturedCourseCard />
-        <FeaturedCourseCard />
+        {courses.map((course) => (
+          <FeaturedCourseCard key={course.id} course={course} />
+        ))}
       </div>
     </section>
   );
--- src/components/ui/FeaturedCourseCard.jsx
+++ src/components/ui/FeaturedCourseCard.jsx
@@ -1,28 +1,25 @@
 import React from "react";
-import coursesBg from "../../assets/images/coursesbg.jpg";
 import star from "../../assets/icons/star.png";
 
-const FeaturedCourseCard = () => {
+const FeaturedCourseCard = ({ course }) => {
   return (
     <div className="w-1/3 p-5 bg-white rounded-xl flex flex-col gap-4 ">
-      <img className="rounded-xl" src={coursesBg} />
+      <img className="rounded-xl h-65 object-cover" src={course.image} />
       <p className="text-sm flex flex-row justify-between items-center text-textgrey">
-        <span>Lecturer Marilyn Mango</span>
+        <span>Lecturer {course.instructor.name}</span>
         <span className="flex flex-row justify-center items-center">
           <img src={star} />
-          4.9
+          {course.avgRating}
         </span>
       </p>
-      <h3 className="text-2xl font-semibold mt-2">
-        Advanced React & TypeScript Development
-      </h3>
+      <h3 className="text-2xl font-semibold mt-2">{course.title}</h3>
       <p className="font-medium text-base text-textgrey">
-        Master modern React patterns, hooks, and TypeScript integration for
-        building scalable web applications.
+        {course.description}
       </p>
       <div className="flex flex-row justify-between mt-2 items-center">
         <p className="text-xs text-textgrey flex flex-row items-center gap-2">
-          Starting From: <span className="text-3xl text-black">$299</span>
+          Starting From:{" "}
+          <span className="text-3xl text-black">${course.basePrice}</span>
         </p>
         <button className="text-xl text-white font-medium py-3 px-6 bg-primary rounded-lg">
           Details
